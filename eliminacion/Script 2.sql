f97c557a30b14582aea3d47acdf3f73ddfd1c78d
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-03-03 11:40:16 -0500

    Ejercicios Eliminar listo

---
+++ eliminacion/Script 2.sql
@@ -0,0 +1,35 @@
+--Script 2
+drop table if exists cuentas
+create table cuentas(
+numero_cuenta char(5) not null,
+cedula_propietario char(5) not null,
+fecha_creacion date not null,
+saldo money not null,
+constraint cuentas_pk primary key(numero_cuenta)
+)
+--Crea y ejecuta 10 inserts
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (67352,'17334','20/07/2025',540.03),
+(86232,'10534','20/06/2025',541.03),
+(67332,'11234','20/05/2025',542.03),
+(67354,'1734','20/04/2025',543.03),
+(67308,'19634','20/03/2025',544.03),
+(76352,'10934','20/02/2025',545.03),
+(64352,'76334','01/01/2026',546.03),
+(43152,'17234','01/03/2026',547.03),
+(79352,'10434','01/04/2026',548.03),
+(79212,'24534','01/02/2026',549.03);
+select * from cuentas
+--Trae el numeron de cuenta y el saldo de todas las cuentas
+select numero_cuenta,saldo from cuentas
+--Trae los registros entre el dia de hoy hasta dos meses antes
+select * from cuentas where fecha_creacion between  '2026/01/01' and '2026/03/01'
+--Trae el numero de cuenta y el saldo de las cuentas desde el dia de hoy hasta 2 mese antes
+select numero_cuenta,saldo from cuentas where fecha_creacion between  '2026/01/01' and '2026/03/01'
+--ACTUALIZAR el saldo a 10 a las cuentas cuya cedula_propiedad empiece por 17
+select * from cuentas;
+update cuentas set saldo = 10 where cedula_propietario like '17%'
+--ELIMINAR todas las cuentas cuya cedula_propietario empieze por 10
+delete from cuentas
+where cedula_propietario like '10%'
+select * from cuentas
